0f3f745198ffce6bceef4777d28c33c6fb223995
UenoSilva <uenosouzasilva@gmil.com>
2024-06-30 14:27:35 -0300

    feat: adicionando o Serializable

--- AppBusSP/app/src/main/java/br/com/aiko/estagio/bussp/data/remote/response/PrevisaoChegadaLinha.kt
+++ AppBusSP/app/src/main/java/br/com/aiko/estagio/bussp/data/remote/response/PrevisaoChegadaLinha.kt
@@ -1,10 +1,14 @@
 package br.com.aiko.estagio.bussp.data.remote.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class PrevisaoChegadaLinha(
     val hr: String,
     val ps: List<PontoParadaLinha>
 )
 
+@Serializable
 data class PontoParadaLinha(
     val cp: Int,
     val np: String,
