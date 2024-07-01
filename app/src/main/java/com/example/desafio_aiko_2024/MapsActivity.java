ad627670388927c7de9e1d3701de8f5c8d2b0b99
Alex Braga <contato@alexbraga.com.br>
2024-07-01 19:56:57 -0300

    Add functionality to display a map view
    This commit adds the `MapsActivity` view and 'Maps SDK for Android' dependency.

---
+++ app/src/main/java/com/example/desafio_aiko_2024/MapsActivity.java
@@ -0,0 +1,51 @@
+package com.example.desafio_aiko_2024;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.desafio_aiko_2024.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sp = new LatLng(-23.5475, -46.63611);
+        mMap.addMarker(new MarkerOptions().position(sp).title("São Paulo"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sp));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(12));
+    }
+}
