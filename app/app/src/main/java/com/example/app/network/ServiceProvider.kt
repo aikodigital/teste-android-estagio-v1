507d159e3e09b6100d52bb861dbccad4093e3263
Daniel Nascimento <pdaniel.contato@gmail.com>
2024-07-02 15:56:06 -0300

    chore: add retrofit configuration

---
+++ app/app/src/main/java/com/example/app/network/ServiceProvider.kt
@@ -0,0 +1,26 @@
+package com.example.app.network
+
+import com.example.app.util.BASE_URL
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ServiceProvider {
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    private var retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <API> createService(apiClass: Class<API>): API = retrofit.create(apiClass)
+}
---
+++ app/app/src/main/java/com/example/app/util/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.app.util
+
+const val BASE_URL = "https://api.olhovivo.sptrans.com.br/v2.1/"
