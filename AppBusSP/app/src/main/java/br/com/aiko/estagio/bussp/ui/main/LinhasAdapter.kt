23265da538f4da3c9a6846272a1c11d65b75b944
UenoSilva <uenosouzasilva@gmil.com>
2024-07-01 22:37:33 -0300

    feat: implementando a buscar por linhas

--- AppBusSP/app/src/main/java/br/com/aiko/estagio/bussp/ui/main/LinhasAdapter.kt
+++ AppBusSP/app/src/main/java/br/com/aiko/estagio/bussp/ui/main/LinhasAdapter.kt
@@ -1,5 +1,7 @@
 package br.com.aiko.estagio.bussp.ui.main
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -7,8 +9,11 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import br.com.aiko.estagio.bussp.data.remote.response.Linha
 import br.com.aiko.estagio.bussp.databinding.ItemLinhaBinding
+import br.com.aiko.estagio.bussp.ui.main.activity.ParadasActivity
 
-class LinhasAdapter : ListAdapter<Linha, LinhasAdapter.LinhasViewHolder>(LinhaDiffCallBack()) {
+class LinhasAdapter(
+    private val context: Context
+) : ListAdapter<Linha, LinhasAdapter.LinhasViewHolder>(LinhaDiffCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LinhasViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -18,6 +23,12 @@ class LinhasAdapter : ListAdapter<Linha, LinhasAdapter.LinhasViewHolder>(LinhaDi
 
     override fun onBindViewHolder(holder: LinhasViewHolder, position: Int) {
         holder.bind(getItem(position))
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ParadasActivity::class.java)
+            intent.putExtra("cl", getItem(position).cl)
+            context.startActivity(intent)
+        }
     }
 
     inner class LinhasViewHolder(private val binding: ItemLinhaBinding) :
--- AppBusSP/app/src/main/java/br/com/aiko/estagio/bussp/ui/main/MainActivity.kt
+++ AppBusSP/app/src/main/java/br/com/aiko/estagio/bussp/ui/main/MainActivity.kt
@@ -1,5 +1,8 @@
 package br.com.aiko.estagio.bussp.ui.main
 
+import android.content.Intent
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -8,60 +11,157 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.viewModelScope
+import androidx.viewpager2.widget.ViewPager2
 import br.com.aiko.estagio.bussp.R
 import br.com.aiko.estagio.bussp.databinding.ActivityMainBinding
+import br.com.aiko.estagio.bussp.databinding.HeaderNavigationDrawerBinding
+import br.com.aiko.estagio.bussp.ui.main.activity.CorredoresActivity
+import br.com.aiko.estagio.bussp.ui.main.activity.EmpresasActivity
+import br.com.aiko.estagio.bussp.ui.main.activity.ParadasActivity
+import br.com.aiko.estagio.bussp.ui.main.adapter.ViewPagerAdapter
+import br.com.aiko.estagio.bussp.ui.main.fragment.MapaFragment
+import br.com.aiko.estagio.bussp.ui.main.viewmodel.AuthenticationViewModel
+import br.com.aiko.estagio.bussp.ui.main.viewmodel.BuscarLinhaViewModel
+import br.com.aiko.estagio.bussp.ui.main.viewmodel.CorredorViewModel
+import br.com.aiko.estagio.bussp.ui.main.viewmodel.EmpresaViewModel
 import br.com.aiko.estagio.bussp.ui.main.viewmodel.ParadasViewModel
+import br.com.aiko.estagio.bussp.ui.main.viewmodel.PosicaoViewModel
+import br.com.aiko.estagio.bussp.ui.main.viewmodel.PrevisaoViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.IOException
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
 
-    private val paradasViewModel: ParadasViewModel by viewModels()
+    private val authenticationViewModel: AuthenticationViewModel by viewModels()
+    private val buscarLinhaViewModel: BuscarLinhaViewModel by viewModels()
+
+
+    private lateinit var linhasAdapter: LinhasAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.authentication.observe(this) { isAuthenticed ->
-            if (isAuthenticed) {
-                binding.tvTeste.text = "True"
-            } else {
-                binding.tvTeste.text = "False"
+        autenticacaoSetup()
+        setupList()
+        setupListener()
+
+        buscarLinhaProximidades()
+        buscarLinhaPorSentido()
+
+    }
+
+
+    private fun autenticacaoSetup() {
+        authenticationViewModel.authentication("5f13bb5bf9366a7a349edf57a769e47421e0d8e9765a307ebb1243bf782dd6b4")
+    }
+
+    private fun buscarLinhaProximidades() {
+        buscarLinhaViewModel.buscarLinha.observe(this) { linhas ->
+            linhasAdapter.submitList(linhas)
+        }
+        buscarLinhaViewModel.buscarLinha(performReverseGeocoding(location))
+
+    }
+
+    private fun buscarLinhaPorSentido() {
+        binding.btnSentidoTp.setOnClickListener {
+            val linha = binding.tilOrigem.editText?.text.toString().trim()
+
+            binding.tvLinhasProximas.text = "Linhas no sentido Terminal Principal"
+
+            buscarLinhaViewModel.buscarLinhaSentido(linha, 1)
+            buscarLinhaViewModel.buscarLinhaSentido.observe(this) { linha ->
+                linhasAdapter.submitList(linha)
             }
         }
 
-        viewModel.authentication("5f13bb5bf9366a7a349edf57a769e47421e0d8e9765a307ebb1243bf782dd6b4")
+        binding.btnSentidoTs.setOnClickListener {
+            val linha = binding.tilOrigem.editText?.text.toString().trim()
 
+            binding.tvLinhasProximas.text = "Linhas no sentido Terminal Secundário"
 
-//        viewModel.buscarLinha.observe(this) { linhas ->
-//            var res = ""
-//            linhas.forEach { l ->
-//                res += "$l \n"
-//            }
-//            binding.tvLinhas.text = res
-//        }
-//        viewModel.buscarLinha("Lapa")
+            buscarLinhaViewModel.buscarLinhaSentido.observe(this) { linha ->
+                linhasAdapter.submitList(linha)
+            }
+            buscarLinhaViewModel.buscarLinhaSentido(linha, 2)
+        }
 
-//        viewModel.buscarLinhaSentido.observe(this) { linhas ->
-//            binding.tvLinhas.text = linhas.toString()
-//        }
-//        viewModel.buscarLinhaSentido("Lapa", 1)
-
-
-//        paradaViewModel.paradas.observe(this) { paradas ->
-//            Log.e("paradaViewModel", paradas.toString())
-//            binding.tvLinhas.text = paradas.toString()
-//        }
-//        paradaViewModel.buscarParada("Afonso")
-
-//        paradasViewModel.paradas.observe(this){ paradas ->
-//            binding.tvLinhas.text = paradas.toString()
-//        }
-//        //paradasViewModel.buscarParadasPorLinha("34041")
-//        paradasViewModel.buscarParadasPorCorredor(8)
     }
+
+    private fun setupList() {
+        linhasAdapter = LinhasAdapter(this)
+        binding.rvLinhasSugeridas.adapter = linhasAdapter
+    }
+
+    private fun setupListener() {
+        binding.topAppBar.setNavigationOnClickListener {
+            binding.main.open()
+        }
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            binding.main.close()
+            true
+        }
+
+        val navigationHeaderBinding = HeaderNavigationDrawerBinding.bind(
+            binding.navigationView.getHeaderView(0)
+        )
+        navigationHeaderBinding.tvLinhas.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        navigationHeaderBinding.tvParadas.setOnClickListener {
+            val intent = Intent(this, ParadasActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        navigationHeaderBinding.tvCorredores.setOnClickListener {
+            val intent = Intent(this, CorredoresActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        navigationHeaderBinding.tvEmpresas.setOnClickListener {
+            val intent = Intent(this, EmpresasActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun performReverseGeocoding(latlng: LatLng): String {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        try {
+            val addresses: List<Address> =
+                geocoder.getFromLocation(latlng.latitude, latlng.longitude, 1)!!
+            if (addresses.isNotEmpty()) {
+                val address: Address = addresses[0]
+                val addressText = address.subLocality
+                return addressText
+                Log.d("performReverseGeocoding", addressText)
+            } else {
+                return ""
+                Log.e("", "Nenhum endereço encontrado")
+            }
+        } catch (e: IOException) {
+            return ""
+            Log.e("performReverseGeocoding exception", e.message.toString())
+        }
+    }
+
+    companion object {
+        val location = LatLng(-23.5354, -46.7178)
+    }
+
 }
+
