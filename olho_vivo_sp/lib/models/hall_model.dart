62c30853ff06158dd6d4de2897eb761b5af33068
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-25 20:49:20 -0300

    feat(models): :sparkles: add hall model

---
+++ olho_vivo_sp/lib/models/hall_model.dart
@@ -0,0 +1,17 @@
+class HallModel {
+  String code;
+  String name;
+
+  HallModel({required this.code, required this.name});
+
+  HallModel.fromMap(Map<String, dynamic> res)
+      : code = res['cc'],
+        name = res['nc'];
+
+  Map<String, Object> toMap() {
+    return {
+      'cc': code,
+      'nc': name,
+    };
+  }
+}
