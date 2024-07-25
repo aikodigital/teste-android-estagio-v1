4fe381ca95b265e983732d21b0c38f093d54aeee
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-25 20:50:16 -0300

    feat(services): :sparkles: add api services class and auth method

---
+++ olho_vivo_sp/lib/services/api_service.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'package:olho_vivo_sp/util/environment.dart';
+
+import '../models/hall_model.dart';
+
+class ApiService {
+  String? _sessionToken;
+
+  Future<Null> authenticate() async {
+    final response = await http.post(
+      Uri.parse(
+        '${Environment.base_url}/${Environment.auth_endpoint}?token=${Environment.token}',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      _sessionToken = response.body;
+    } else {
+      throw Exception(Environment.failed_api_auth_msg);
+    }
+  }
+}
