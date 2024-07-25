89a785f8e864ce344a2af966252ec0ade315672c
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-25 19:02:02 -0300

    feat(widgets): :sparkles: add bottom navigation widget and function to change current index

--- olho_vivo_sp/lib/widgets/bottom_navigation_widget.dart
+++ olho_vivo_sp/lib/widgets/bottom_navigation_widget.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
-class BottomNavigationWidget extends StatelessWidget {
+class BottomNavigationWidget extends StatefulWidget {
   const BottomNavigationWidget({
     super.key,
   });
 
   @override
+  State<BottomNavigationWidget> createState() => _BottomNavigationWidgetState();
+}
+
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  int _currentIndex = 0;
+
+  final _items = [
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.search),
+      label: 'Linhas',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(
+        Icons.directions_bus,
+      ),
+      label: 'Veículos no mapa',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(
+        Icons.dangerous,
+      ),
+      label: 'Paradas',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(
+        Icons.route,
+      ),
+      label: 'Corredores',
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: 0,
-      items: const [],
-      onTap: (value) {},
+      currentIndex: _currentIndex,
+      items: _items,
+      onTap: (value) {
+        setState(() {
+          _currentIndex = value;
+        });
+      },
     );
   }
 }
