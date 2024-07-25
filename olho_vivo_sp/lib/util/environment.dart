1fec2954ce93dadb6b8309134fe229c86304ed82
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-25 20:50:40 -0300

    feat(util): :sparkles: add environment class with variables

---
+++ olho_vivo_sp/lib/util/environment.dart
@@ -0,0 +1,7 @@
+abstract class Environment {
+  static const failed_api_auth_msg = "Falha ao realizar autenticação";
+
+  static const base_url = "";
+  static const token = "";
+  static const auth_endpoint = "";
+}
