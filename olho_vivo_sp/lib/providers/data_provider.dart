c8880c053e40b76ed05983aed5e4a2f1fffee821
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-26 09:55:40 -0300

    feat(providers): :sparkles: add auth method to data provider

---
+++ olho_vivo_sp/lib/providers/data_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:olho_vivo_sp/services/api_service.dart';
+
+class DataProvider extends ChangeNotifier {
+  final ApiService apiService;
+
+  DataProvider({required this.apiService});
+
+  Future<Null> authenticate() async => await apiService.authenticate();
+}
