79eb0f4d0c36b032d625a52ef59af3043fc5377e
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-26 09:56:18 -0300

    feat(util): :recycle: changed fail msg var name

--- olho_vivo_sp/lib/util/environment.dart
+++ olho_vivo_sp/lib/util/environment.dart
@@ -1,5 +1,5 @@
 abstract class Environment {
-  static const failed_api_auth_msg = "Falha ao realizar autenticação";
+  static const failed_auth_msg = "Falha ao realizar autenticação";
 
   static const base_url = "";
   static const token = "";
