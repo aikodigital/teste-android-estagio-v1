838e521018d5c9f5420135d9c43c2b548a120628
vitororigens <vitorabreufjb@hotmail.com>
2024-07-26 19:52:45 -0300

    arrumando home

--- App.tsx
+++ App.tsx
@@ -1,46 +1,24 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, FlatList, ActivityIndicator } from 'react-native';
-import useOlhoVivoAPI from './src/hooks';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+//
+import { Routes } from './src/routes';
+import theme from './src/theme';
+import { ActivityIndicator } from 'react-native';
 
-const BusStopsScreen: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const { busStops, loading, error, fetchBusStops } = useOlhoVivoAPI();
 
-  const handleSearch = () => {
-    fetchBusStops(searchTerm);
-  };
-
-  if (loading) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
-  }
-
-  if (error) {
-    return <Text>Erro: {error.message}</Text>;
-  }
+export default function App() {
+  const [fontLoader] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
 
   return (
-    <View>
-      <TextInput
-        value={searchTerm}
-        onChangeText={setSearchTerm}
-        placeholder="Buscar paradas"
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
-      />
-      <Button title="Buscar" onPress={handleSearch} />
-      <FlatList
-        data={busStops}
-        keyExtractor={(item) => item.cp.toString()}
-        renderItem={({ item }) => (
-          <View>
-            <Text>Nome: {item.np}</Text>
-            <Text>Endereço: {item.ed}</Text>
-            <Text>Latitude: {item.py}</Text>
-            <Text>Longitude: {item.px}</Text>
-          </View>
-        )}
-      />
-    </View>
+    <ThemeProvider theme={theme}>
+        {fontLoader ? <Routes /> : <ActivityIndicator />}
+        <StatusBar style="auto" />
+    </ThemeProvider>
   );
-};
+}
 
-export default BusStopsScreen;
