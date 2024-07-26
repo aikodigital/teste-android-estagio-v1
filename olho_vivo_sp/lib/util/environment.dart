3edc8c95b7907897f2f6a635c748496e7ed1254c
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-26 15:03:03 -0300

    feat(util): :sparkles: add env var values

--- olho_vivo_sp/lib/util/environment.dart
+++ olho_vivo_sp/lib/util/environment.dart
@@ -1,7 +1,10 @@
 abstract class Environment {
-  static const failed_auth_msg = "Falha ao realizar autenticação";
+  static const failed_auth_msg = 'Falha ao realizar autenticação.';
+  static const failed_get_halls_msg = 'Falha ao obter corredores.';
 
-  static const base_url = "";
-  static const token = "";
-  static const auth_endpoint = "";
+  static const base_url = 'https://aiko-olhovivo-proxy.aikodigital.io';
+  static const token =
+      'aa4bbfd91dd0321d0f886554e9bcccfbbcc00341d85b3f5bff4b563ce93efc30';
+  static const auth_endpoint = '/Login/Autenticar';
+  static const halls_endpoint = '/Corredor';
 }
