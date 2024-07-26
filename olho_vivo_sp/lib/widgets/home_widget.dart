19f0e1cd846275528bd7ef7d47b2bea99fbb93b2
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-26 15:02:22 -0300

    feat(widgets): :sparkles: changed home to stateful widget, added screens and pass function callback to bottom nav

--- olho_vivo_sp/lib/widgets/home_widget.dart
+++ olho_vivo_sp/lib/widgets/home_widget.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:olho_vivo_sp/widgets/hall_list.dart';
 import 'bottom_navigation_widget.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _screens = [
+    const HallList(),
+    const Placeholder(),
+    const Placeholder(),
+  ];
+  int _index = 0;
+
+  void changeScreen(int value) {
+    setState(() {
+      _index = value;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return Scaffold(
+      body: _screens[_index],
+      bottomNavigationBar: BottomNavigationWidget(
+        onChangeScreen: changeScreen,
       ),
-      bottomNavigationBar: BottomNavigationWidget(),
     );
   }
 }
