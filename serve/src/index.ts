4c1998ac07ee394f5d16af3ede3259f5d067c2ab
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-07-25 23:31:20 -0300

    chore: add dependeces in server side

---
+++ serve/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
