43044b4094c751b6eb93afae216c02dc43bd0525
jramuss <josuedasilvaramus@hotmail.com>
2024-07-26 01:08:51 -0400

    criando o getRoute

--- sp_movement/lib/app/modules/bus_route/repositories/bus_route_repository.dart
+++ sp_movement/lib/app/modules/bus_route/repositories/bus_route_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:sp_movement/app/core/repository/app_repository.dart';
+import 'package:sp_movement/app/modules/bus_route/models/vehicle_model.dart';
 import 'package:sp_movement/app/modules/bus_route/models/vehiclelocation_model.dart';
 
 class BusRouteRepository {
@@ -7,11 +8,8 @@ class BusRouteRepository {
 
   static Future<List<VehicleLocationModel>> getPosition() async {
     dynamic response =
-        await AppRepository.getDio()!.get(AppRepository.baseUrl + endpoint,
-            options: AppRepository.getOptionsHttpSSL(
-              Headers.jsonContentType,
-              AppRepository.token,
-            ));
+        await AppRepository.getDio()!.get('${AppRepository.baseUrl}$endpoint?token=${AppRepository.token}');
+    
     print(response.data);
     List<VehicleLocationModel> list = [];
     if (response != null) {
@@ -25,7 +23,21 @@ class BusRouteRepository {
     }
   }
 
-  static void getRoute() {}
+  static  Future<List<VehicleModel>> getRoute() async {
+
+    dynamic response = AppRepository.getDio()!.get('${AppRepository.baseUrl}$endpoint/Linha?token=${AppRepository.token}');
+    print(response.data);
+    List<VehicleModel> list = [];
+    if(response != null){
+      dynamic responseJson = AppRepository.returnResponse(response);
+      responseJson.data['vs'].forEach((v){
+        list.add(VehicleModel.fromJson(v));
+      });
+      return list;
+    } else {
+      return [];
+    }
+  }
 
   static void getGarage() {}
 }
