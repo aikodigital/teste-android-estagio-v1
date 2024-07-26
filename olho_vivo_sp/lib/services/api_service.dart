0ec1b94d514f2f01cd5ceb3ccb4d07437c21e623
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-26 15:04:11 -0300

    feat(services): :sparkles: api service extends change notifier and fix get halls

--- olho_vivo_sp/lib/services/api_service.dart
+++ olho_vivo_sp/lib/services/api_service.dart
@@ -1,22 +1,68 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:olho_vivo_sp/util/environment.dart';
 
 import '../models/hall_model.dart';
 
-class ApiService {
+class ApiService extends ChangeNotifier {
   String? _sessionToken;
 
   Future<Null> authenticate() async {
-    final response = await http.post(
-      Uri.parse(
-        '${Environment.base_url}/${Environment.auth_endpoint}?token=${Environment.token}',
-      ),
-    );
+    try {
+      final response = await http.post(
+        Uri.parse(
+          '${Environment.base_url}${Environment.auth_endpoint}?token=${Environment.token}',
+        ),
+      );
 
-    if (response.statusCode == 200) {
-      _sessionToken = response.body;
-    } else {
-      throw Exception(Environment.failed_auth_msg);
+      if (response.statusCode >= HttpStatus.ok &&
+          response.statusCode < HttpStatus.multipleChoices) {
+        _sessionToken = response.body;
+      } else {
+        throw Exception(Environment.failed_auth_msg);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<HallModel>> getHalls() async {
+    try {
+      if (_sessionToken == null) {
+        await authenticate();
+      }
+
+      final response = await http.get(
+        Uri.parse(
+          '${Environment.base_url}${Environment.halls_endpoint}',
+        ),
+        headers: {
+          'Authorization': 'Bearer $_sessionToken',
+        },
+      );
+
+      print(response.body);
+
+      if (response.statusCode >= HttpStatus.ok &&
+          response.statusCode < HttpStatus.multipleChoices) {
+        List<dynamic> data = jsonDecode(response.body);
+
+        return data
+            .map(
+              (hall) => HallModel(
+                code: hall['cc'],
+                name: hall['cn'],
+              ),
+            )
+            .toList();
+      } else {
+        throw Exception(Environment.failed_get_halls_msg);
+      }
+    } catch (e) {
+      rethrow;
     }
   }
 }
