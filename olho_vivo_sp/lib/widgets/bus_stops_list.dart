928e01c2f5f97e0262b92367c624bfb297d22894
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-27 16:14:05 -0300

    feat(widgets): :sparkles: add bus stops list

---
+++ olho_vivo_sp/lib/widgets/bus_stops_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:olho_vivo_sp/models/bus_stop_model.dart';
+import 'package:olho_vivo_sp/widgets/bus_stop_list_item.dart';
+
+class BusStopsList extends StatelessWidget {
+  final List<BusStopModel> busStops;
+
+  const BusStopsList({super.key, required this.busStops});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.only(
+        left: 20,
+        right: 20,
+        top: 8,
+      ),
+      itemBuilder: (ctx, i) {
+        return BusStopListItem(busStop: busStops[i]);
+      },
+      itemCount: busStops.length,
+    );
+  }
+}
