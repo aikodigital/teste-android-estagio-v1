61e2f05aa8350cb032b9cc0d1282b3892075d6d2
vitosnatios <vitosdeveloper@gmail.com>
2024-07-27 02:13:05 -0300

    buscar-linha-de-onibus funcionando

--- app/(tabs)/buscar-linha-de-onibus.tsx
+++ app/(tabs)/buscar-linha-de-onibus.tsx
@@ -1,78 +1,125 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { useEffect, useRef, useState } from 'react';
-import {
-  Linha,
-  LinhaParaPosicao,
-  PosicaoDosVeiculos,
-  PosicaoVeiculo,
-} from '@/types/posicao';
-import MapView, { MapMarker } from 'react-native-maps';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Linha } from '@/types/posicao';
 import { autenticarNaApi } from '@/helpers/autenticarNaApi';
-import De from '@/components/pages/posicaoDosVeiculos/De';
-import Para from '@/components/pages/posicaoDosVeiculos/Para';
 import Loading from '@/components/form/loading/Loading';
-import useRodeEmIntervalo from '@/custom-hooks/useRodeEmIntervalo';
-
+import ErrorComponent from '@/components/form/error/Error';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
 export type Trajeto = { de: string | null; para: string | null };
 
 export default function PaginaDeLinhaDeOnibus() {
   const [linhas, setLinhas] = useState<Linha[]>([]);
+  const [inputDePesquisa, setInputDePesquisa] = useState('');
+  const [formState, setFormState] = useState({ loading: false, error: '' });
+  const timeoutRef = useRef<NodeJS.Timeout>();
 
   const fetchData = async () => {
     try {
+      await autenticarNaApi();
       const res = await fetch(
-        process.env.API_URL + '/Linha/Buscar?termosBusca=8000'
+        process.env.API_URL + '/Linha/Buscar?termosBusca=' + inputDePesquisa
       );
+      if (!res.ok) throw new Error('Houve um erro ao pesquisar.');
       const json = (await res.json()) as Linha[];
-      console.log(json.length);
+      if (!json.length) throw new Error(`Nenhuma linha encontrada.`);
       setLinhas(json);
     } catch (error) {
-      error instanceof Error && console.log(error.message);
       await autenticarNaApi();
-      await fetchData();
+      if (error instanceof Error) {
+        console.log(error.message);
+        setFormState((prev) => ({ ...prev, error: error.message }));
+      }
+    } finally {
+      setFormState((prev) => ({ ...prev, loading: false }));
     }
   };
-  useRodeEmIntervalo(fetchData, 10000);
 
-  if (!linhas?.length) return <Loading />;
+  const lidarComPesquisa = () => {
+    setFormState({ error: '', loading: true });
+    setLinhas([]);
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(async () => {
+      await fetchData();
+    }, 2000);
+  };
+
   return (
     <View style={styles.pageContainer}>
       <Text style={styles.title}>Buscar Linhas de Ônibus</Text>
-      <FlatList
-        style={styles.itemsContainer}
-        data={linhas}
-        renderItem={({ item }) => {
-          let sl: string;
-          switch (item.sl) {
-            case 1:
-              sl = 'Principal para Secundário';
-              break;
-            case 2:
-              sl = 'Secundário para Principal';
-              break;
-            default:
-              sl = '';
-              break;
-          }
-          return (
-            <View style={styles.item}>
-              <Text>Código Identificador: {item.cl}</Text>
-              <Text>Terminal Principal: {item.tp}</Text>
-              <Text>Terminal Secundário: {item.ts}</Text>
-              <Text>Sentido: {sl}</Text>
-              <Text>Operação: {item.lc ? 'Circular' : 'Terminal'}</Text>
-            </View>
-          );
-        }}
-      />
+      <View style={styles.container}>
+        <EvilIcons style={styles.icon} name='search' size={24} color='black' />
+        <TextInput
+          style={styles.input}
+          placeholder='Ex: 8000'
+          value={inputDePesquisa}
+          onChangeText={setInputDePesquisa}
+          onChange={lidarComPesquisa}
+        />
+      </View>
+      {formState.error && <ErrorComponent messagem={formState.error} />}
+      {formState.loading && !formState.error ? (
+        <Loading />
+      ) : (
+        inputDePesquisa.length > 0 && (
+          <>
+            <Text style={styles.title}>{linhas[0].lt}</Text>
+            <FlatList
+              data={linhas}
+              renderItem={({ item }) => {
+                let sl: string;
+                switch (item.sl) {
+                  case 1:
+                    sl = 'Principal para Secundário';
+                    break;
+                  case 2:
+                    sl = 'Secundário para Principal';
+                    break;
+                  default:
+                    sl = '';
+                    break;
+                }
+                return (
+                  <View style={styles.item}>
+                    <Text>
+                      Linha: {item.lt} - {item.tl}
+                    </Text>
+                    <Text>Código Identificador: {item.cl}</Text>
+                    <Text>Terminal Principal: {item.tp}</Text>
+                    <Text>Terminal Secundário: {item.ts}</Text>
+                    <Text>Sentido: {sl}</Text>
+                    <Text>Operação: {item.lc ? 'Circular' : 'Terminal'}</Text>
+                  </View>
+                );
+              }}
+            />
+          </>
+        )
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  pageContainer: { paddingTop: 28, paddingHorizontal: 14 },
+  pageContainer: {
+    paddingTop: 28,
+    paddingBottom: 100,
+    paddingHorizontal: 14,
+  },
   title: { textAlign: 'center' },
-  itemsContainer: {},
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#F2F2F2',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
+  icon: { marginBottom: 4 },
+  input: {
+    flex: 1,
+    height: 40,
+    fontSize: 16,
+  },
   item: {
     borderWidth: 1,
     borderRadius: 6,
---
+++ components/form/error/Error.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ErrorComponent = ({ messagem }: { messagem: string }) => {
+  return (
+    <View style={styles.containerDeErro}>
+      <Text style={styles.mensagemDeErro}>{messagem}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerDeErro: {
+    backgroundColor: '#f2dede', // Cor de fundo para indicar erro
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  mensagemDeErro: {
+    color: '#a94442', // Cor do texto de erro
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default ErrorComponent;
