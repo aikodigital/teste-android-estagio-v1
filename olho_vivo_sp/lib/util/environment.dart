488ff4b6fcef1f7be52262390a0b600095e7e679
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-27 15:11:40 -0300

    feat(util): :sparkles: add var types

--- olho_vivo_sp/lib/util/environment.dart
+++ olho_vivo_sp/lib/util/environment.dart
@@ -1,10 +1,13 @@
 abstract class Environment {
-  static const failed_auth_msg = 'Falha ao realizar autenticação.';
-  static const failed_get_halls_msg = 'Falha ao obter corredores.';
+  static const String failed_auth_msg = 'Falha ao realizar autenticação.';
+  static const String failed_get_halls_msg = 'Falha ao obter corredores.';
 
-  static const base_url = 'https://aiko-olhovivo-proxy.aikodigital.io';
-  static const token =
+  static const String base_url = 'https://aiko-olhovivo-proxy.aikodigital.io';
+  static const String token =
       'aa4bbfd91dd0321d0f886554e9bcccfbbcc00341d85b3f5bff4b563ce93efc30';
-  static const auth_endpoint = '/Login/Autenticar';
-  static const halls_endpoint = '/Corredor';
+  static const String auth_endpoint = '/Login/Autenticar';
+  static const String halls_endpoint = '/Corredor';
+  static const String bus_stops_endpoint = '/Parada/Buscar';
+  static const String bus_stops_by_hall_endpoint =
+      '/Parada/BuscarParadasPorCorredor';
 }
