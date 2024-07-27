bdaeddbfc1a612523e6381ba981d7223bbb68293
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-27 19:40:16 -0300

    feat(util): :sparkles: add map detail screen route

--- olho_vivo_sp/lib/util/routes.dart
+++ olho_vivo_sp/lib/util/routes.dart
@@ -1,4 +1,4 @@
 abstract class Routes {
-  static const String hall_detail_screen = '/hall-detail-screen';
+  static const String map_detail_screen = '/map-detail-screen';
   static const String bus_stops_screen = '/bus-stops-screen';
 }
