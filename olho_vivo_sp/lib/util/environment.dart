50934a4029293d860d00d6094457b145a1414336
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-27 19:39:21 -0300

    feat(util): :sparkles: added fail msgs to obtain arrival forecast, bus stop and arrival forecast by bus stop endpoint to env var

--- olho_vivo_sp/lib/util/environment.dart
+++ olho_vivo_sp/lib/util/environment.dart
@@ -1,6 +1,10 @@
 abstract class Environment {
   static const String failed_auth_msg = 'Falha ao realizar autenticação.';
   static const String failed_get_halls_msg = 'Falha ao obter corredores.';
+  static const String failed_get_arrival_forecast =
+      'Falha ao obter previsões de chegada.';
+  static const String failed_get_bus_stops =
+      'Falha ao obter paradas de ônibus.';
 
   static const String base_url = 'https://aiko-olhovivo-proxy.aikodigital.io';
   static const String token =
@@ -10,5 +14,6 @@ abstract class Environment {
   static const String bus_stops_endpoint = '/Parada/Buscar';
   static const String bus_stops_by_hall_endpoint =
       '/Parada/BuscarParadasPorCorredor';
-  static const String arrival_forecast_endpoint = '/Previsao';
+  static const String arrival_forecast_by_bus_stop_endpoint =
+      '/Previsao/Parada';
 }
