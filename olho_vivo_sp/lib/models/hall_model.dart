cac26f43ba0f3a6dd48be865b548f32ed3350cf4
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-27 15:11:18 -0300

    feat(models): :sparkles: remove toMap method

--- olho_vivo_sp/lib/models/hall_model.dart
+++ olho_vivo_sp/lib/models/hall_model.dart
@@ -7,11 +7,4 @@ class HallModel {
   HallModel.fromMap(Map<String, dynamic> res)
       : code = res['cc'],
         name = res['nc'];
-
-  Map<String, Object> toMap() {
-    return {
-      'cc': code,
-      'nc': name,
-    };
-  }
 }
