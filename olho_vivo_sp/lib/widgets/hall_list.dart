a5bcbb89373e354696c2f3cf8f4474fe784b9f9c
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-27 19:47:34 -0300

    feat(widgets): :sparkles: add sort method to halls

--- olho_vivo_sp/lib/widgets/hall_list.dart
+++ olho_vivo_sp/lib/widgets/hall_list.dart
@@ -17,6 +17,16 @@ class HallList extends StatelessWidget {
         if (snp.hasData && snp.connectionState != ConnectionState.active) {
           List<HallModel> halls = snp.data!;
 
+          halls.sort((h1, h2) {
+            if (h1.code > h2.code) {
+              return 1;
+            } else if (h1.code < h2.code) {
+              return -1;
+            }
+
+            return 0;
+          });
+
           return ListView.builder(
             padding: const EdgeInsets.only(
               left: 20,
