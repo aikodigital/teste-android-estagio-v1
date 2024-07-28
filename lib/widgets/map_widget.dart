302b37d49677a64cece390a419af972f1c3d75d9
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-28 10:35:19 -0300

    refactor(widgets): :recycle: add ?? operator to set first mark insted of target when is null

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends StatelessWidget {
+  final LatLng? target;
   final Set<Marker> markers;
 
-  const MapWidget({super.key, required this.markers});
+  const MapWidget({super.key, required this.markers, this.target});
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       initialCameraPosition: CameraPosition(
-        target: markers.first.position,
+        target: target ?? markers.first.position,
         zoom: 12,
       ),
       markers: markers,
