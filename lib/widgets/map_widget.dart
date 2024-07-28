9917e6d61a087ac0c908f2c62af792ac5cc7ab4a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-07-28 16:53:30 -0300

    feat(widgets): :sparkles: add search text field

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -4,17 +4,52 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapWidget extends StatelessWidget {
   final LatLng? target;
   final Set<Marker> markers;
+  final bool enableSearch;
+  final String searchLabel;
 
-  const MapWidget({super.key, required this.markers, this.target});
+  const MapWidget({
+    super.key,
+    required this.markers,
+    this.target,
+    this.enableSearch = true,
+    this.searchLabel = 'Pesquisar',
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: target ?? markers.first.position,
-        zoom: 12,
-      ),
-      markers: markers,
+    return Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: target ?? markers.first.position,
+            zoom: 12,
+          ),
+          markers: markers,
+        ),
+        Visibility(
+          visible: enableSearch,
+          child: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.10,
+            color: Theme.of(context).cardColor,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 20,
+              ),
+              child: TextField(
+                onSubmitted: (value) {},
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.search),
+                  label: Text(
+                    searchLabel,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
