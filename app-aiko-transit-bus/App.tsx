d425050663e3aca5ab58d7bcf5f42790b70fe4b4
douglas sales <douglas-8545@hotmail.com>
2024-07-27 22:24:05 -0300

    ref remove imports

--- app-aiko-transit-bus/App.tsx
+++ app-aiko-transit-bus/App.tsx
@@ -1,21 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
-import {
-  GlobalContext,
-  GlobalContextProvider,
-} from "./src/context/GlobalContext";
+import { GlobalContextProvider } from "./src/context/GlobalContext";
 import Routes from "./src/Routes";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Splash } from "./src/pages/Splash/Splash";
-import { useEffect, useState } from "react";
-
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <GlobalContextProvider>
-            <Routes />
-          </GlobalContextProvider>
-        </NavigationContainer>
+      <NavigationContainer>
+        <GlobalContextProvider>
+          <Routes />
+        </GlobalContextProvider>
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
