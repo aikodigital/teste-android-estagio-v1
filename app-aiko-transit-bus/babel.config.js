dbb0db609e02b37b6735c61083604ea1db0dfdc4
douglas sales <douglas-8545@hotmail.com>
2024-07-28 20:37:18 -0300

    feat: added env in babelConfig

--- app-aiko-transit-bus/babel.config.js
+++ app-aiko-transit-bus/babel.config.js
@@ -1,7 +1,22 @@
-module.exports = function(api) {
+const path = require('path');
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["nativewind/babel"],
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: path.resolve(__dirname, '.env'),
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
