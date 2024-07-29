e94b34383dafae5c5dbd7293945fa02d44788ef4
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2024-07-29 11:16:50 -0400

    Delete config/services directory

--- config/services/ConfigAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const ConfigAPI = axios.create({
-  baseURL: 'https://aiko-olhovivo-proxy.aikodigital.io/', //Proxy fornecida pela Aiko
-  //baseURL_old: 'http://api.olhovivo.sptrans.com.br/v2.1' //Nao estava funcionando no momento da implementacao
-});
-
-export default ConfigAPI;
