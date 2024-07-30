4b46970efc45ff1ddc697a05f4cf5c688e772863
carlansouza <carlanogueirasouza@gmail.com>
2024-07-30 09:42:25 -0300

    att autheticate

--- api/authContext.jsx
+++ api/authContext.jsx
@@ -1,37 +1,20 @@
-import React, { createContext, useContext, useState } from 'react';
 import axios from 'axios';
 
 const API_URL = 'https://api.olhovivo.sptrans.com.br/v2.1';
 const API_KEY = 'b7c59cb9ae96c66937e6c61c274206493316c5c21105db7b64cbcbd0672cdbc4';
 
-const AuthContext = createContext();
+export const authenticate = async () => {
+  try {
+    const response = await axios.post(`${API_URL}/Login/Autenticar`, null, {
+      params: { token: API_KEY },
+    });
 
-export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const authenticate = async () => {
-    try {
-      const response = await axios.post(`${API_URL}/Login/Autenticar`, null, {
-        params: { token: API_KEY },
-      });
-      if (response.status === 200) {
-        setIsAuthenticated(true);
-        return true;
-      } else {
-        throw new Error('Falha na autenticação');
-      }
-    } catch (error) {
-      console.error('Erro ao autenticar:', error);
-      setIsAuthenticated(false);
-      return false;
+    if (response.data) {
+      return true;
     }
-  };
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, authenticate }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  } catch (error) {
+    console.error('Erro na autenticação:', error);
+  }
+  
+  return false;
 };
-
-export const useAuth = () => useContext(AuthContext);
